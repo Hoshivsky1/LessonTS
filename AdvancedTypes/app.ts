3e45378f0b5e58487b2ea44ad3a1225ddcccd9d7
Maxim <96632620+Hoshivsky1@users.noreply.github.com>
2022-08-14 19:45:55 +0300

    Advanced Types - Union

--- AdvancedTypes/app.ts
+++ AdvancedTypes/app.ts
@@ -1,1 +1,32 @@
-console.log('hello');
+//!Union====================================================
+function logoId(id:string | number | boolean) {
+    if(typeof id === 'string') {
+        console.log(id)
+    } else if (typeof id === 'number'){
+        console.log(id)
+    }
+}
+
+function logError(err: string | string[]) {
+    if(Array.isArray(err)) {
+        console.log(err)
+    } else {
+        console.log(err);
+    }
+}
+
+function logObj(obj: {a: number}  | {b: number}) {
+    if('a' in obj) {
+        console.log(obj.a)
+    } else {
+        console.log(obj.b);
+    }
+}
+
+function logMlt(a:string | number, b:string | boolean) {
+    if (a===b) {
+
+    }
+}
+
+//!Literal Types===============================================
